6a4026406c1e46385c68abb9c26b2bdd6465cddf
Lorenzo <hello@lorenzo0111.me>
2025-07-01 12:51:11 +0200

    Removed HexiTextLib

--- src/main/java/me/lorenzo0111/rocketplaceholders/utilities/HexParser.java
+++ src/main/java/me/lorenzo0111/rocketplaceholders/utilities/HexParser.java
@@ -24,18 +24,47 @@
 
 package me.lorenzo0111.rocketplaceholders.utilities;
 
-import com.haroldstudios.hexitextlib.HexResolver;
+import net.md_5.bungee.api.ChatColor;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HexParser {
     private static final Pattern PATTERN = Pattern.compile("&(#[a-fA-F0-9]{6})");
 
-    private HexParser() {}
-
-    public static @NotNull String text(@NotNull String text) {
-        return HexResolver.parseHexString(text, PATTERN);
+    private HexParser() {
     }
 
+    public static @NotNull String text(@NotNull String text) {
+        return parseHexString(text, PATTERN);
+    }
+
+    private static boolean serverSupportsHex() {
+        try {
+            ChatColor.of(Color.BLACK);
+            return true;
+        } catch (NoSuchMethodError ignore) {
+            return false;
+        }
+    }
+
+    private static String parseHexString(@NotNull String text, @NotNull Pattern hexPattern) {
+        Matcher hexColorMatcher = hexPattern.matcher(text);
+
+        if (serverSupportsHex()) {
+            while (hexColorMatcher.find()) {
+                String hex = hexColorMatcher.group(1);
+                ChatColor color = ChatColor.of(hex);
+
+                String before = text.substring(0, hexColorMatcher.start());
+                String after = text.substring(hexColorMatcher.end());
+                text = before + color + after;
+                hexColorMatcher = hexPattern.matcher(text);
+            }
+        }
+
+        return org.bukkit.ChatColor.translateAlternateColorCodes('&', text);
+    }
 }
